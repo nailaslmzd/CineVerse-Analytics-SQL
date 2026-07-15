f391cbf5d54c658d4a68db603a78b333652e70b8
Naila <146843637+nailaslmzd@users.noreply.github.com>
2026-07-15 17:07:54 +0400

    Create insert_data.sql

---
+++ insert_data.sql
@@ -0,0 +1,45 @@
+/*
+=========================================
+ CineVerse Analytics
+ Insert Data
+=========================================
+*/
+
+-- ======================================
+-- Studios
+-- ======================================
+
+INSERT INTO studios (studio_name, headquarters)
+VALUES
+('Nova Pictures', 'Los Angeles'),
+('Aurora Films', 'London'),
+('Titan Entertainment', 'New York'),
+('Eclipse Studios', 'Toronto'),
+('Phoenix Productions', 'Sydney');
+
+-- ======================================
+-- Actors
+-- ======================================
+
+INSERT INTO actors (first_name, last_name, country, birth_year)
+VALUES
+('Emma','Stone','USA',1988),
+('Ryan','Gosling','Canada',1980),
+('Margot','Robbie','Australia',1990),
+('Zendaya','Coleman','USA',1996),
+('Timothee','Chalamet','USA',1995),
+('Florence','Pugh','England',1996),
+('Keanu','Reeves','Canada',1964),
+('Scarlett','Johansson','USA',1984),
+('Cillian','Murphy','Ireland',1976),
+('Ana','de Armas','Cuba',1988),
+('Tom','Holland','England',1996),
+('Jennifer','Lawrence','USA',1990),
+('Robert','Downey Jr.','USA',1965),
+('Chris','Evans','USA',1981),
+('Pedro','Pascal','Chile',1975),
+('Sydney','Sweeney','USA',1997),
+('Jenna','Ortega','USA',2002),
+('Henry','Cavill','England',1983),
+('Gal','Gadot','Israel',1985),
+('Leonardo','DiCaprio','USA',1974);
